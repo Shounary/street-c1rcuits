26de6b7a8de42057f3e479cf03279236639b2a87
Mikhail Romanenko <romanenko.xr@gmail.com>
2025-12-10 19:28:34 -0800

    testing a new table entry

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Todo.create({ content: window.prompt("Todo content"), isDone: !!window.prompt("Todo isDone?")});
   }
 
   function deteleTodo(id: string) {
