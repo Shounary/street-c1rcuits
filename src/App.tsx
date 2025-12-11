31eae00339a28b4b94b44fb1de24c3d939c99732
Mikhail Romanenko <romanenko.xr@gmail.com>
2025-12-10 20:56:33 -0800

    testing trackSegments

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,15 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
+async function fetchTrackSegments() {
+  const { data: trackSegments } = await client.models.TrackSegment.list()
+  console.log(trackSegments)
+  return trackSegments
+}
+
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [trackSegments, ] = useState<Array<Schema["TrackSegment"]["type"]>>([]);
+  const [trackSegments, setTrackSegments] = useState<Array<Schema["TrackSegment"]["type"]>>([]);
   const { user, signOut } = useAuthenticator();
 
   
@@ -15,6 +21,11 @@ function App() {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+    const fetchData = async () => {
+      const fetchedTrackSegments = await fetchTrackSegments();
+      setTrackSegments(fetchedTrackSegments);
+    };
+    fetchData();
   }, []);
 
   function createTodo() {
@@ -40,7 +51,7 @@ function App() {
       <h1>Track Segments</h1>
       <ul>
         {trackSegments.map((trackSegment) => (
-          <li key={trackSegment.id}>{[trackSegment.name, trackSegment.shape]}</li>
+          <li key={trackSegment.id}>{trackSegment.shape}</li>
         ))}
       </ul>
       <button onClick={ signOut }>Sign Out</button>
