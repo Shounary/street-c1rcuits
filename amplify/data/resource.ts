645e83aa630fb4f6eb7635c85055d141cfb0ee14
Mikhail Romanenko <romanenko.xr@gmail.com>
2025-12-18 18:38:10 -0800

    fixed segment schema to include an array of params

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -19,7 +19,7 @@ const schema = a.schema({
       id: a.id(),
       name: a.string(),
       type: a.enum(["straight", "arc", "clothoid", "compound"]),
-      parameterSchema: a.json(),
+      parameterSchema: a.json().array(),
       parameters: a.json(),
       metadata: a.customType({
         description: a.string(),
